a0a2c5763e3c5f9a2e82f53454a33167b11e04d3
lotfi-hadjsadok <lotfihadjsadok@gmail.com>
2024-02-21 01:00:57 +0100

    Update authors info

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 /**
  * Plugin Name: Woo Facebook Pixels & Messenger
  * Description: With this plugin you will be able to create multiple facebook pixels in one website and use the messenger chat functionality.
- * Author: L.Hadjsadok / Y.Bellouche
- * Author URI: https://www.linkedin.com/in/lotfi-hadjsadok-6599571ba
+ * Author: L.Hadjsadok
+ * Author URI: https://www.facebook.com/lotfihadjsadok.dev
  * Version : 1.0.1
  */
 
@@ -16,4 +16,4 @@ if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 }
 
 use Inc\App;
-(new App)->start();
+( new App() )->start();
